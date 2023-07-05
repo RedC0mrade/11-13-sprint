f873267d6b13292e3e5945da83dcbf43a1d3b77a
Бутенко Ниолай <e2e4e2e4@mail.ru>
2023-07-05 17:29:10 +0200

    21

--- calculate.py
+++ calculate.py
@@ -1,30 +1,44 @@
+# 88835324
 from operator import add, floordiv, mul, sub
-
-operators = {'+': add, '-': sub, '*': mul, '/': floordiv}
 
 
 class Stack:
 
     def __init__(self):
-        self.dek = []
-        self.head = 0
-        self.operations = []
+        """Параметры Стэка"""
+        self.stack = []
 
-    def polish_notation(self, notation: str):
+    def push(self, data: int):
+        """Добавление элемента"""
+        return self.stack.append(data)
 
-        list(filter(lambda x: self.dek.append(int(x)) if x.isdigit() or (
-                x.startswith('-') and x[1:].isdigit()) else
-                self.operations.append(x), notation.split()))
-        self.head = len(self.dec) - 1
-        for operation in self.operations:
-            if self.head > 1:
-                self.dek[self.head - 1] = operators[operation](
-                    self.dek[self.head - 1], self.dek[self.head])
-                self.head -= 1
-            else:
-                return self.dek[self.head]
-        return self.dek[0]
+    def pop(self):
+        """Возващение и удаление последнего элемента"""
+        if len(self.stack) == 0:
+            return None
+        return self.stack.pop()
 
 
-calculator = Stack()
-print(calculator.polish_notation(input()))
+def polish_notation(notation: str):
+    """Решение польской нотации"""
+    stack = Stack()
+    operators = {'+': add, '-': sub, '*': mul, '/': floordiv}
+    for i in notation:
+        if i in operators:
+            last_data = stack.pop()
+            pre_last_data = stack.pop()
+            result = operators[i](pre_last_data, last_data)
+            stack.push(result)
+        else:
+            stack.push(int(i))
+    return stack.pop()
+
+
+def main():
+    """Основная функция"""
+    notation = input().split()
+    print(polish_notation(notation))
+
+
+if __name__ == '__main__':
+    main()
---
+++ dec.py
@@ -0,0 +1,76 @@
+# 88836972
+class QueueOverflowError(Exception):
+    """Queue overflow."""
+
+
+class EmptyListError(Exception):
+    """Empty list error."""
+
+
+class Deque:
+    def __init__(self, number_limit: int):
+        """Атрибуты очереди"""
+        self.__gosh_queue = [None] * number_limit
+        self.__max_number_limit = number_limit
+        self.__head = -1
+        self.__tail = 0
+        self.__size = 0
+
+    def push_back(self, value):
+        """Добаавление значения в начало очереди"""
+        if self.__size == self.__max_number_limit:
+            raise EmptyListError
+        self.__tail = self.__queue_count_function(self.__tail, -1)
+        self.__gosh_queue[self.__tail] = value
+        self.__size += 1
+
+    def push_front(self, value):
+        """Добаавление значения в начало очереди"""
+        if self.__size == self.__max_number_limit:
+            raise EmptyListError
+        self.__head = self.__queue_count_function(self.__head, 1)
+        self.__gosh_queue[self.__head] = value
+        self.__size += 1
+
+    def pop_front(self):
+        """Получение и удаление значения первого значения очереди"""
+        if self.__size == 0:
+            raise EmptyListError
+        data = self.__gosh_queue[self.__head]
+        self.__gosh_queue[self.__head] = None
+        self.__head = self.__queue_count_function(self.__head, -1)
+        self.__size -= 1
+        return data
+
+    def pop_back(self):
+        """Получение и удаление значения последнего значения очереди"""
+        if self.__size == 0:
+            raise EmptyListError
+        data = self.__gosh_queue[self.__tail]
+        self.__gosh_queue[self.__tail] = None
+        self.__tail = self.__queue_count_function(self.__tail, 1)
+        self.__size -= 1
+        return data
+
+    def __queue_count_function(self, item: int, num: int):
+        """Вспомогательная переменная для подсчета места значения в очереди"""
+        return (item + num) % self.__max_number_limit
+
+
+def main():
+    """Основная функция"""
+    num = int(input())
+    gosh_structure = Deque(int(input()))
+    input_commands = [input().split() for _ in range(num)]
+    for i in input_commands:
+        try:
+            if len(i) == 1:
+                print(getattr(gosh_structure, i[0])())
+            else:
+                getattr(gosh_structure, i[0])(int(i[1]))
+        except EmptyListError or QueueOverflowError:
+            print('error')
+
+
+if __name__ == '__main__':
+    main()
