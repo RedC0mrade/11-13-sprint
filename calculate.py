ee825f1507b01af3eb70ce5b558465cedbaa19f4
Бутенко Николай <e2e4e2e4@mail.ru>
2023-07-23 18:52:38 +0200

    22

--- calculate.py
+++ calculate.py
@@ -1,5 +1,9 @@
-# 88835324
+# 88863240
 from operator import add, floordiv, mul, sub
+
+
+class EmptyStackError(Exception):
+    """Empty stack error."""
 
 
 class Stack:
@@ -8,14 +12,14 @@ class Stack:
         """Параметры Стэка"""
         self.stack = []
 
-    def push(self, data: int):
+    def push(self, data: int) -> None:
         """Добавление элемента"""
         return self.stack.append(data)
 
-    def pop(self):
+    def pop(self) -> int:
         """Возващение и удаление последнего элемента"""
-        if len(self.stack) == 0:
-            return None
+        if not self.stack:
+            raise EmptyStackError
         return self.stack.pop()
 
 
@@ -25,10 +29,13 @@ def polish_notation(notation: str):
     operators = {'+': add, '-': sub, '*': mul, '/': floordiv}
     for i in notation:
         if i in operators:
-            last_data = stack.pop()
-            pre_last_data = stack.pop()
-            result = operators[i](pre_last_data, last_data)
-            stack.push(result)
+            try:
+                last_data = stack.pop()
+                pre_last_data = stack.pop()
+                result = operators[i](pre_last_data, last_data)
+                stack.push(result)
+            except EmptyStackError:
+                return "Не корректный ввод данных"
         else:
             stack.push(int(i))
     return stack.pop()
