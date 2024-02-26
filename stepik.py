dd5baac2edd161ac47ad58e1e1adaa1f7484697f
RedC0mrade <e2e4e2e4@mai.ru>
2024-02-26 15:22:54 +0200

    132

--- stepik.py
+++ stepik.py
@@ -1,13 +1,1 @@
-def dict_travel(nested_dicts, path=[]):
-    for k in nested_dicts:
-        if isinstance(nested_dicts[k], dict):
-            m = dict(sorted(nested_dicts[k].items()))
-            dict_travel(m, path + [k])
-        else:
-
-            print(f'{".".join(path + [k])}: {nested_dicts[k]}')
-
-
-data = {'b': {'c': 30, 'a': 10, 'b': {'d': 40, 'e': 50}}}
-
-dict_travel(data)
+print(*[chr(i) for i in range(97, 123)], sep='\n')
