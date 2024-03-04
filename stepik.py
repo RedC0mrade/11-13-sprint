b1e52a8391c4328b0703a6b8106033749937818b
RedC0mrade <e2e4e2e4@mai.ru>
2024-03-04 13:06:55 +0200

    132

--- stepik.py
+++ stepik.py
@@ -1,4 +1,9 @@
-def convert(number):
-    return bin(number)[2:], oct(number)[2:], hex(number)[2:]
+def zip_longest(*args, fill=None):
+    lent = len(sorted(args, key=len)[-1])
+    result = []
+    for i in args:
+        result.append(i + (lent-len(i))*[fill])
+    return list(zip(*result))
 
-print(convert(-24))
+
+print(zip_longest([1, 2, 3, 4, 5], ['a', 'b', 'c'], fill='_'))
