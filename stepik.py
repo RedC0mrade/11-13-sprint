4f7b089d6b19803b1fe2bcf1086fbcab568fc7ba
RedC0mrade <e2e4e2e4@mai.ru>
2024-03-08 19:41:53 +0200

    132

--- stepik.py
+++ stepik.py
@@ -1,9 +1,3 @@
-def zip_longest(*args, fill=None):
-    lent = len(sorted(args, key=len)[-1])
-    result = []
-    for i in args:
-        result.append(i + (lent-len(i))*[fill])
-    return list(zip(*result))
+data = ['Timur', -16.648911695768902, 'six', -202, 883.0093275936454, -765, (3, 4), -105.10718000213546, 976, -308.96857946288094, 458, ['one', 'two'], 479.92207220345927, -87, -71, 'twelve', 112, -621, -715.0179551194733, 'seven', 229, 729, -358, [1, 2, 3], -974, 882, -894.4709033242768, '', 323.7720806756133, 'beegeek', -224, 431, 170.6353248658936, -343.0016746052049, 'number', 104.17133679352878, [], -353.5964777099863, 'zero', -113, 288, None, -708.3036176571618]
 
-
-print(zip_longest([1, 2, 3, 4, 5], ['a', 'b', 'c'], fill='_'))
+print(*(map(lambda x: int(x), filter(lambda x: type(x) in (int, float), data))), sep='\n')
